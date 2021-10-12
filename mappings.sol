6e2755ab46828bf3652b8a27c7c047be2006d397
Arthurgallina1 <arthur.gallina@gmail.com>
2021-10-11 22:47:26 -0300

    add nested mapping

--- mappings.sol
+++ mappings.sol
@@ -1,3 +1,6 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+
 contract MyContract {
     // Mappings
     mapping(uint => string) public names;
@@ -8,7 +11,7 @@ contract MyContract {
         string author;
     }
 
-    constructor() public {
+    constructor() {
         names[1] = "Adam";
         names[2] = "Bruce";
         names[3] = "Carl";
@@ -17,4 +20,14 @@ contract MyContract {
     function addBook(uint _id, string memory _title, string memory _author) public {
         books[_id] = Book(_title, _author);
     }
+    
+    
+    //nested mapping
+    //Creating ownership (via address) to a Book
+    mapping(address => mapping(uint => Book)) public myBooks;
+    
+     function addMyBook(uint _id, string memory _title, string memory _author) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
+    }
+    
 }
