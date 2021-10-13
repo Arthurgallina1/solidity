c8cc3c843cb2a4a018502253ac44e7453528e660
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-10-13 20:44:05 -0300

    add basic convertions

---
+++ conversion.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Conversion {
+
+    //uint = uint256 (0 - 2^256-1)
+    uint number = 2;
+    
+    //Converter 32 bits pra 16 bits
+    //Conversão para numeros menores perde os bits de maior ordem.
+    uint32 a = 0x12345678;
+    uint16 b = uint16(a); // b = 0x5678
+    
+    //16 pra 32
+    //Conversão para ordem maior adiciona paddings bit a esquerda.
+    uint16 c = 0x1234;
+    uint32 d = uint32(c); // d = 0x0001234
+
+    // What is cost?
+    // conversar para bytes menores custa 
+    bytes2 e = 0x1234;
+    bytes1 f = bytes(1); //f = 0x12
+    
+    bytes2 g = 0x1234;
+    bytes4 h = bytes4(g); // h = 0x1234000
+
+}
