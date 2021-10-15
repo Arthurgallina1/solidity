27f1a0b780263c32ac2cdc96b28f48abcd5ce246
Arthurgallina1 <arthur.gallina@gmail.com>
2021-10-15 08:46:58 -0300

    add special variables to ledger

--- ledgerBalance.sol
+++ ledgerBalance.sol
@@ -15,11 +15,34 @@ contract LedgerBalance {
 }
 
 contract Updated {
-    // access my address from another contract
+    
     function updatedBalance() public {
         LedgerBalance ledger = new LedgerBalance();        
         ledger.updateBalance(30);
     }
     
+    
+}
+
+contract SimpleStoroge {
+    
+    uint storedData;
+    
+    function set(uint x) public {
+        // find block dificulty 
+        //storedData = block.difficulty;
+        
+        //find time which we`re running this function
+        // storedData = block.timestamp;
+
+        //grab block number
+        storedData = block.number;
+        
+        // storedData = x;
+    }
+    
+    function get() public view returns (uint) {
+        return storedData;       
+    }
     
 }
